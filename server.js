4aa90c4fac419c031bb86858fce1919a474092af
mohamedTheesan21 <mohamedtheesan4@gmail.com>
2023-10-04 09:11:37 +0530

    server created

---
+++ server.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "///index.html");
+});
+
+app.post("/",function(req,res){
+    // Handle the login logic here
+  const email = req.body.email;
+  const password = req.body.password;
+});
+
+app.listen(3000);
