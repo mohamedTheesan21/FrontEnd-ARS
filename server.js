6cdcf99fea72050decaf6a75dd40f68a450385a8
mohamedTheesan21 <mohamedtheesan4@gmail.com>
2023-10-04 09:12:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname + "///index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/",function(req,res){
