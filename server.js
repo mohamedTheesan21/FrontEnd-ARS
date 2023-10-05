2b28f40a5b4320a8685fb60bc1a0cb0970b246c4
mohamedTheesan21 <mohamedtheesan4@gmail.com>
2023-10-05 09:34:56 +0530

    add Bootstrap

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
